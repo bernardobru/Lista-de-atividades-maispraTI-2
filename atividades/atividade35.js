2858c92c743c48ed1611fc9f057cf88991e7c7d7
bernardobru <tutibruxel@gmail.com>
2024-06-12 12:35:53 -0300

    reverted to a previous commit

--- atividades/atividade35.js
+++ atividades/atividade35.js
@@ -1,30 +1,36 @@
 function criaVetor(tamanho) {
     const vetor = new Array();
     for (let i = 0; i < tamanho; i ++) {
-        vetor[i] = Math.round(Math.random() * 50);
+        vetor[i] = Math.round(Math.random() * 50) + 1;
     }
     return vetor;
 }
 function parOuImpar(vetor) {
+    console.log(`Vetor Original: ${vetor}`);
     let vetorPar = new Array();
-    let vetorImpar = new Array;
-    for (let i = 0; i < vetor.length; i++) {
-        if (vetor[i] % 2 == 0) {
-            vetorPar.push(vetor[i]);
+    let vetorImpar = new Array();
+    for (let valores of vetor) {
+        if (valores % 2 == 0) {
+            vetorPar.push(valores);
+            if (vetorPar.length == 5) {
+                console.log(`Vetor de Pares: ${vetorPar}`);
+                vetorPar = new Array();
+            }
         } else {
-            vetorImpar.push(vetor[i]);
+            vetorImpar.push(valores);
+            if (vetorImpar.length == 5) {
+                console.log(`Vetor de Ímpares: ${vetorImpar}`);
+                vetorImpar = new Array();
+            }
         }
-        if (vetorPar.length == 5) {
-            console.log(`${vetorPar}`);
-            vetorPar = new Array();
-        }
-        if (vetorImpar.length == 5) {
-            console.log(`${vetorImpar}`);
-            vetorImpar = new Array();            
-        }
+    }
+    if (vetorPar.length > 0) {
+        console.log(`Vetor de Pares: ${vetorPar}`);
+    }
+    if (vetorImpar.length > 0) {
+        console.log(`Vetor de Ímpares: ${vetorImpar}`);
     }
     return;
 }
-const vetorDe30 = criaVetor(30);
-console.log(`${vetorDe30}`)
-console.log(parOuImpar(vetorDe30));
+const vetor = criaVetor(30);
+parOuImpar(vetor); 
